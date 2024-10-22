e53d37db441ff4f75371d247d2d0f7a8a7d17687
cs-ice <1992192799@qq.com>
2024-10-22 13:03:45 +0800

    完善了calcValue的实现，添加并缩进了注释，增加可读性
    具体：增加了单牌对子三张的value实现
    完成了顺子同花等的value实现

--- src/Cardgroup.py
+++ src/Cardgroup.py
@@ -54,20 +54,21 @@ def show(card_id):                  # 翻译ID成牌
 class Cardgroup:
     def __init__(self):
         self.cards = []                         # 储存卡牌ID, 整型
-        self.type =  INVALID                           # -1无效 0为单 1为对 2为三 etc.
+        self.type =  INVALID                    # -1无效 0为单 1为对 2为三 etc.
         self.deter_ID = -1                      # 牌组大小的决定性手牌ID
         self.value = -1                         # 对于三张和五张牌的大小评估，同type才比较
         pass
     
     def add_card(self, card_id):                   # 往卡组添加牌，并update类型
         self.cards.append(card_id)
-        # self.judgeType()                        # update
-        # self.update_cardpoints(card.point)
+        self.judgeType()                           # update
+        self.calcValue()                           # 计算VALUE
 
-    def sort(self):                             # 排序 一般用于手牌 依据card的num降序排序
+
+    def sort(self):                                # 排序 一般用于手牌 依据card的num降序排序
         self.cards.sort(key=lambda x: x.num, reverse=True)
 
-    def remove_card(self, card):                # 移除牌 并update
+    def remove_card(self, card):                   # 移除牌 并update
         if(card in self.cards):
             self.cards.remove(card)
             self.judgeType()
@@ -80,29 +81,27 @@ class Cardgroup:
         '''
     def isSTRAIGHT(self):   
         temp_points = []                            # 临时列表 储存一组点数
-        maxpoint_id = -1                        # 初始化为第一张牌的id, 记录最大点数对应的ID，以求它的花色
-        max_point = -1                          # 最大点数, 并初始化
+        maxpoint_id = -1                            # 初始化为第一张牌的id, 记录最大点数对应的ID，以求它的花色
+        max_point = -1                              # 最大点数, 并初始化
         current_point = -1                          # 这两者都是临时变量，分别是上一个和当前点数
         for id in self.cards:
             current_point = get_point(id) 
             if(current_point == 11):                 # 11是卡牌2的id, 顺子不包含卡牌2
                 return False
-            elif(current_point == 12):              # !!! 3开头的顺子是最小的, 所以这里处理为0
+            elif(current_point == 12):               # !!! 3开头的顺子是最小的, 所以这里处理为0
                 current_point = 0
             else:
-                current_point += 1                  # 因为卡牌3在顺子中为0, 所以卡牌4等需要+1
+                current_point += 1                   # 因为卡牌3在顺子中为0, 所以卡牌4等需要+1
             
             temp_points.append(current_point)
-            if(current_point > max_point):         # 如果当前点数较大，则保存ID
+            if(current_point > max_point):           # 如果当前点数较大，则保存ID
                 maxpoint_id = id
                 max_point = current_point
 
-                
+        temp_points.sort()                              # 升序排序
 
-        temp_points.sort()                      # 升序排序
-
-        for i in range(4):                      # 长度为5，最大索引为4，即3+1
-            if(temp_points[i+1] == temp_points[i] + 1): # 后一位比前一位大一
+        for i in range(4):                              # 长度为5，最大索引为4，即3+1
+            if(temp_points[i+1] == temp_points[i] + 1): # 后一位比前一位大一，就是顺子
                 continue
             else:
                 return False                            # 否则就不是顺子
@@ -124,7 +123,7 @@ class Cardgroup:
             current_point = get_point(id)
             if(current_point > prev_point):         # 如果当前点数较大，则保存ID
                 maxpoint_id = id
-                prev_point = current_point              # 将上一个点数更新为当前点数
+                prev_point = current_point          # 将上一个点数更新为当前点数
             
             prev_suit = current_suit                # 将上一个花色更新为当前花色
 
@@ -145,11 +144,11 @@ class Cardgroup:
         if(len(key_lst) != 2):                      # 点数超过两种，肯定不是三带对
             return False
         key1, key2 = get_point(key_lst[0]), get_point(key_lst[1])               # key变量是点数
-        if (element_count[key1] == 3) and (element_count[key2] == 2):            # 点数重复三次，说明是三带对
+        if (element_count[key1] == 3) and (element_count[key2] == 2):           # 点数重复三次，说明是三带对
             self.deter_ID = key_lst[0]                          
             return True
-        elif (element_count[key1] == 2) and (element_count[key2] == 3):            # 重复三次，说明是三带对
-            self.deter_ID = key_lst[1]                                              # 决定ID为出现三次的卡牌点数
+        elif (element_count[key1] == 2) and (element_count[key2] == 3):         # 重复三次，说明是三带对
+            self.deter_ID = key_lst[1]                                          # 决定ID为出现三次的卡牌点数 只需要点数即可 不需要花色
             return True
         return False        
         
@@ -162,28 +161,32 @@ class Cardgroup:
             else:
                 element_count[elem] = 1             # 
             if element_count[elem] == 4:            # 重复4次，说明是四代一
-                self.deter_ID = id                  # 决定ID 为重复四次那张牌
+                self.deter_ID = id                  # 决定ID 为重复四次那张牌 只需要点数即可 不需要花色
                 return True
 
         return False
 
         '''
         judgeType()
-        功能：判断当前牌组(self.cards)的类型
+        功能：判断当前牌组(self.cards)的类型, 同时获取决定性ID
         参数: 无, 需要使用self.cards, 不会改变列表
-        返回值: 无, 但是会改变self.type的值
+        返回值: 无, 但是会改变self.type, self.deter_ID的值
         实现: 先判断长度, 在判具体类型
         '''
-    
-    def judgeType(self):                        # 判断类型
+    def judgeType(self):                                                    # 判断类型 同时取得决定性ID以算取value
         length = len(self.cards)
+        self.type = INVALID
+        self.deter_ID = -1                                                  # 每次判断前 先重置Type和deterID
+        self.value = -1
 
         if(length == 1):
             self.type = SINGLE
+            self.deter_ID = self.cards[0]
         elif(length == 2):
-            p1, p2 = get_point(self.cards[0]), get_point(self.cards[1])       # p1p2临时储存点数
+            p1, p2 = get_point(self.cards[0]), get_point(self.cards[1])     # p1p2临时储存点数
             if(p1 == p2):
                 self.type = DOUBLE
+                self.deter_ID = max(self.cards[0], self.cards[1])
             else:
                 self.type = INVALID
         elif(length == 3):
@@ -191,6 +194,7 @@ class Cardgroup:
             p3 = get_point(self.cards[2])
             if((p1 == p2) and (p1 == p3)):
                 self.type = THREE
+                self.deter_ID = max(self.cards[0], self.cards[1], self.cards[2])
             else:
                 self.type = INVALID
         elif(length == 4):
@@ -202,7 +206,7 @@ class Cardgroup:
             '''
             if(self.isFLUSH() and self.isSTRAIGHT()):   # 先判同花顺，因为它和顺子，同花判断重合
                 self.type = FLUSHSTRAIGHT
-            elif (self.isSTRAIGHT()):                     # 判断顺子
+            elif (self.isSTRAIGHT()):                   # 判断顺子
                 self.type = STRAIGHT
             elif (self.isFLUSH()):                      # 判断同花
                 self.type = FLUSH
@@ -219,38 +223,40 @@ class Cardgroup:
     作用: 在已知type后, 计算牌组的分数
     参数: 无, 需要用到self.type, self.deter_ID
     返回值: 无, 通过self.value传值
+    一般用在judgeType()的后面
     '''
-    def calcValue(self):
-        sum = 0
+    def calcValue(self):                    # 为了代码的一致性，self.value是由scores赋值的 所以请修改scores
+        scores = 0
         point = get_point(self.deter_ID)
         suit = get_suit(self.deter_ID)
         if(self.type == INVALID):
-            self.value = -1
+            scores = -1                     # 无效类型 分数是-1
         elif(self.type == SINGLE):
-            pass                                # 
+            scores = self.deter_ID          # 单牌分数即ID
         elif(self.type == DOUBLE):
-            pass
-        elif(self.type == THREE):
-            pass
-        elif(self.type == STRAIGHT):
-            pass
+            scores = self.deter_ID          # 对子即取大的ID，因为大的ID对应大的花色
+        elif(self.type == THREE):                                           
+            scores = self.deter_ID          # 三张同理   
+        elif(self.type == STRAIGHT):        # # 因为34567最小，所以方片7(id 12)为1分
+            scores = self.deter_ID          # 取决于ID 那VALUE就是ID
 
         elif(self.type == FLUSH):           # 从方片到梅花, 分数加13, 方片9为最低分1分
             while (suit > 0):
-                sum += 13
+                scores += 13
                 suit -= 1
-            while point >= 5:                 # point 5 即 卡牌9
-                sum += 1
+            while point >= 5:               # point 5 即 卡牌9
+                scores += 1
                 point -= 1
 
         elif(self.type == THREETWO):        # 三带一只看点数, 所以分数即是点数+1, 规定最低分是1
-            sum = get_point(self.deter_ID) + 1
+            scores = get_point(self.deter_ID) + 1
         elif(self.type == FOURONE):         # 四带一同理
-            sum = get_point(self.deter_ID) + 1
+            scores = get_point(self.deter_ID) + 1
         elif(self.type == FLUSHSTRAIGHT):   
             pass
         
-        # print(sum)
+        self.value = scores
+        # print(scores)
 
     def showcard(self):
         if self.type == INVALID:
@@ -277,7 +283,7 @@ class Cardgroup:
 
 
 
-
+""" 
 while True:
     group = Cardgroup()
     i = int(input('输入几张牌？'))
@@ -288,8 +294,18 @@ while True:
     show(group.deter_ID)
     print()
     group.calcValue()
-    group.showcard()                            # 测试代码区
-    print()
-        
+    print('Value:', str(group.value))
+    group.showcard()                            # 测试代码区 """
+    # print()
+group = Cardgroup()
+arr = [9, 51,50, 8, 11,]
+for i in arr:
+    group.add_card(i)
+print("决定牌:", end="")
+show(group.deter_ID)
+print()
+print('Value:', group.value)
+group.showcard() 
 
-# updated 23点22分
+
+# updated 2024年10月22日12点58分
