b0339a52c5bacd00716ac81b831a87a5da31dbe4
cs-ice <1992192799@qq.com>
2024-10-23 13:33:22 +0800

    简单修整 缩进等 无关紧要的push

--- src/Cardgroup.py
+++ src/Cardgroup.py
@@ -56,18 +56,18 @@ FLUSHSTRAIGHT = 8    # 同花顺
 
 
 def get_point(card_id): 
-    return card_id // 4            # 返回0是卡牌4， 返回12是卡牌3
+    return card_id // 4                         # 返回0是卡牌4， 返回12是卡牌3
 
 def get_suit(card_id):
-    return card_id %  4           # 3spade, 2heart, 1club, 0diamond
+    return card_id %  4                         # 3spade, 2heart, 1club, 0diamond
 
-def create_cardgroup(card_lst) -> Cardgroup:     # 一个构造函数 给定一个列表返回一个Cg对象
+def create_cardgroup(card_lst):                 # 一个构造函数 给定一个列表返回一个Cg对象
     cg = Cardgroup()
     for id in card_lst:
         cg.add_card(id)
     return cg
 
-def create_cardgroup_by_tuple(card_tuple_lst):   # 给一个元组(花色, 点数)列表 返回一个Cg对象
+def create_cardgroup_by_tuple(card_tuple_lst):  # 给一个元组(花色, 点数)列表 返回一个Cg对象
     cg = Cardgroup()
     for i in card_tuple_lst:
         card_suit, card_point = i
@@ -136,7 +136,7 @@ class Cardgroup:
         传入另一个Cardgroup 判断本身是否包含这个cg
         考虑到卡牌ID是唯一的 使用python内置的set来实现
     '''
-    def contain(self, cg: Cardgroup) -> bool:     
+    def contain(self, cg) -> bool:     
         # 简便实现 不考虑变成set后长度变短问题 因为不可能有一模一样的牌的
         return set(cg.cards).issubset(set(self.cards))
 
@@ -405,6 +405,4 @@ while True:
     print('Value:', group.value)
     group.showcard()
 
-
-
-# updated 2024年10月22日12点58分
+# updated 2024年10月23日13点28分
